20d5718f164dfc0972049d52fe84a54af3ce43d8
badr2e <badreprocontact@gmail.com>
2025-01-30 10:07:05 +0100

    added id, name of pokemon and styles for pokemon pages

--- app/pokemon/[id].tsx
+++ app/pokemon/[id].tsx
@@ -1,11 +1,40 @@
-import { Text, View } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
+import { RootView } from "@/components/RootView";
+import { Row } from "@/components/Row";
+import { ThemedText } from "@/components/ThemedText";
+import { useFetchQuery } from "@/hooks/useFetchQuery";
 
 export default function Pokemon() {
-  const params = useLocalSearchParams();
+  const params = useLocalSearchParams() as { id: string };
+  const { data: pokemon } = useFetchQuery("/pokemon/[id]", { id: params.id });
   return (
-    <View>
+    <RootView>
+      <Row style={styles.header}>
+        <Pressable onPress={router.back}>
+          <Row gap={8}>
+            <Image
+              source={require("@/assets/images/back.png")}
+              width={32}
+              height={32}
+            />
+            <ThemedText color="grayWhite" variant="headline">
+              {pokemon?.name}
+            </ThemedText>
+          </Row>
+        </Pressable>
+        <ThemedText color="grayWhite" variant="subtitle2">
+          #{params.id.padStart(3, "0")}
+        </ThemedText>
+      </Row>
       <Text>Pokemon {params.id}</Text>
-    </View>
+    </RootView>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    margin: 20,
+    justifyContent: "space-between",
+  },
+});
