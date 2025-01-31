281a4ed6f9a044f720c6e51474f39c28db378b86
badr2e <badreprocontact@gmail.com>
2025-01-31 01:56:58 +0100

    added infinite navigation between pokemon

--- app/pokemon/[id].tsx
+++ app/pokemon/[id].tsx
@@ -17,15 +17,89 @@ import { PokemonType } from "@/components/pokemon/PokemonType";
 import { PokemonSpec } from "@/components/pokemon/PokemonSpec";
 import { PokemonStat } from "@/components/pokemon/PokemonStat";
 import { Audio } from "expo-av";
+import PagerView from "react-native-pager-view";
+import { useRef, useState } from "react";
 
 export default function Pokemon() {
-  const colors = useThemeColors();
   const params = useLocalSearchParams() as { id: string };
-  const { data: pokemon } = useFetchQuery("/pokemon/[id]", { id: params.id });
+  const [id, setId] = useState(parseInt(params.id, 10));
+  const offset = useRef(1);
+  const pager = useRef<PagerView>(null);
+
+  const onPageSelected = (e: { nativeEvent: { position: number } }) => {
+    offset.current = e.nativeEvent.position - 1;
+  };
+
+  const onPageScrollStateChanged = (e: {
+    nativeEvent: { pageScrollState: string };
+  }) => {
+    if (e.nativeEvent.pageScrollState !== "idle") {
+      return;
+    }
+    if (offset.current === -1 && id === 2) {
+      return;
+    }
+    if (offset.current === 1 && id === 150) {
+      return;
+    }
+    if (offset.current !== 0) {
+      setId(id + offset.current);
+      offset.current = 0;
+      pager.current?.setPageWithoutAnimation(1);
+    }
+  };
+
+  const onNext = () => {
+    pager.current?.setPage(2 + offset.current);
+  };
+  const onPrevious = () => {
+    pager.current?.setPage(0);
+  };
+  return (
+    <PagerView
+      ref={pager}
+      onPageSelected={onPageSelected}
+      onPageScrollStateChanged={onPageScrollStateChanged}
+      initialPage={1}
+      style={{ flex: 1 }}
+    >
+      <PokemonView
+        key={id - 1}
+        id={id - 1}
+        onNext={onNext}
+        onPrevious={onPrevious}
+      />
+      <PokemonView key={id} id={id} onNext={onNext} onPrevious={onPrevious} />
+      <PokemonView
+        key={id + 1}
+        id={id + 1}
+        onNext={onNext}
+        onPrevious={onPrevious}
+      />
+    </PagerView>
+  );
+}
+
+type Props = {
+  id: number;
+  onPrevious: () => void;
+  onNext: () => void;
+};
+
+function PokemonView({
+  id,
+  onNext,
+  onPrevious,
+}: {
+  id: number;
+  onNext: () => void;
+  onPrevious: () => void;
+}) {
+  const colors = useThemeColors();
+  const { data: pokemon } = useFetchQuery("/pokemon/[id]", { id: id });
   const { data: species } = useFetchQuery("/pokemon-species/[id]", {
-    id: params.id,
+    id: id,
   });
-  const id = parseInt(params.id, 10);
   const mainType = pokemon?.types?.[0].type.name;
   const colorType = mainType ? Colors.type[mainType] : colors.tint;
   const types = pokemon?.types ?? [];
@@ -46,19 +120,6 @@ export default function Pokemon() {
       { shouldPlay: true }
     );
     sound.playAsync();
-  };
-
-  const onPrevious = () => {
-    router.replace({
-      pathname: "/pokemon/[id]",
-      params: { id: Math.max(id - 1, 1) },
-    });
-  };
-  const onNext = () => {
-    router.replace({
-      pathname: "/pokemon/[id]",
-      params: { id: Math.min(id + 1, 151) },
-    });
   };
 
   const isFirst = id === 1;
@@ -91,7 +152,7 @@ export default function Pokemon() {
             </Row>
           </Pressable>
           <ThemedText color="grayWhite" variant="subtitle2">
-            #{params.id.padStart(3, "0")}
+            #{id.toString().padStart(3, "0")}
           </ThemedText>
         </Row>
 
@@ -112,7 +173,7 @@ export default function Pokemon() {
               <Image
                 style={styles.artwork}
                 source={{
-                  uri: getPokemonArtwork(params.id),
+                  uri: getPokemonArtwork(id),
                 }}
                 width={200}
                 height={200}
